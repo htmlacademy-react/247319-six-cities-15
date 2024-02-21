de788d05ae4b054cbd418122b2548a48c597921a
Roman Milaev <r.milaev@yandex.ru>
2024-02-22 01:54:58 +0300

    исправил именование для not-found page и поменял расположение

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,7 +5,7 @@ import MainPage from '../../pages/main-page/main-page';
 import LoginPage from '../../pages/login-page/login-page';
 import FavoritesPage from '../../pages/favorites-page/favorites-page';
 import OfferPage from '../../pages/offer-page/offer-page';
-import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
